1c88075c771766c5bd3c5a8e579a706f10f99bf2
macbook <macbook@MacBook-Pro.local>
2017-01-16 11:01:55 -0700

    angular practice

---
+++ main.controller.js
@@ -0,0 +1,10 @@
+angular.module("app").controller("main", ['$scope', 'mainService', function($scope, mainService) {
+//when button clicked, will call getTrumpQuote method on service
+$scope.trumpQuoteWanted = function() {
+//now calling service method
+    mainService.getTrumpQuote().then(function(response) {
+//assign response to $scope
+        $scope.quote = response;
+    })
+};
+}]);
---
+++ main.service.js
@@ -0,0 +1,14 @@
+angular.module("app").service("mainService", ['$http', function($http) {
+return {
+//method to call Random Trump Quote API
+    getTrumpQuote: function() {
+//now calling API endpoint
+        return $http.get("https://api.whatdoestrumpthink.com/api/v1/quotes/random").then(function(response) {
+//wait for response then return to caller
+            return response.data.message;
+        })
+    }
+
+}
+
+}]);
